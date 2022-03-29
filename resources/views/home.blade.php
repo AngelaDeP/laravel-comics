73ebd8639f05a1d6644b67a384a15f134f9b993f
Angela De Pasquale <angeladepasquale_@libero.it>
2022-03-30 01:07:02 +0200

    Inizio ricreazione layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.base')
+
+@section('pageTitle', 'Home')
+
+@section('content')
+    CONTENUTO 
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,59 @@
+<header>
+    <div class="top-header">
+        <div class="top-header-text">
+            <ul>
+                <li>
+                    <a href="#">
+                        DC POWER&trade; VISA&reg;
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        ADDITIONAL DC SITES
+                    </a>
+                </li>
+            </ul>
+        </div>       
+    </div>
+    <div class="header-section">
+        <div class="container">
+            <div class="header-logo">
+                <img src="../assets/dc-logo.png" alt="">
+            </div>
+            <div class="header-nav">
+                <ul class="navbar">
+                    <li>
+                        <a href="#">CHARACTERS</a>
+                    </li>
+                    <li>
+                        <a href="#">COMICS</a>
+                    </li>
+                    <li>
+                        <a href="#">MOVIES</a>
+                    </li>
+                    <li>
+                        <a href="#">TV</a>
+                    </li>
+                    <li>
+                        <a href="#">GAMES</a>
+                    </li>
+                    <li>
+                        <a href="#">COLLECTIBLES</a>
+                    </li>
+                    <li>
+                        <a href="#">VIDEOS</a>
+                    </li>
+                    <li>
+                        <a href="#">FANS</a>
+                    </li>
+                    <li>
+                        <a href="#">NEWS</a>
+                    </li>
+                    <li>
+                        <a href="#">SHOP</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
